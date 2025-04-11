62794877b48fb97b4bd8d9ee6d5be686db054eb5
Mariano B. Vidal <marianobazalarvidal.229958@alumnos.info.unlp.edu.ar>
2025-04-11 14:09:07 -0300

    Subido

---
+++ 5to Semestre/CPLP/Práctica 4/ejercicio14.c
@@ -0,0 +1,29 @@
+// archivo1.C
+int v1;
+int *a;
+Int fun2()
+{ int v1, y;
+    for(y=0; y<8; y++)
+    { extern int v2;
+    }
+}
+main()
+{static int var3;
+    extern int v2;
+    int v1, y;
+    for(y=0; y<10; y++)
+    { char var1='C';
+        a=&v1;}
+}
+
+// archivo2.C
+    static int aux;
+    int v2;
+    static int fun2()
+    { extern int v1;
+        aux=aux+1;
+    }
+    int fun3()
+    { int aux;
+        aux=aux+1;
+    }
