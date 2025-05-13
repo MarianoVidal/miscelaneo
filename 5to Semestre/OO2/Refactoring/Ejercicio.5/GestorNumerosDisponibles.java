784bb6bd96e36dffaf64f1d358097d4d0fe8232d
Mariano B. Vidal <marianobazalarvidal.229958@alumnos.info.unlp.edu.ar>
2025-05-13 00:21:15 -0300

    Subidos

---
+++ 5to Semestre/OO2/Refactoring/Ejercicio.5/GestorNumerosDisponibles.java
@@ -0,0 +1,28 @@
+package ar.edu.unlp.info.oo2.facturacion_llamadas;
+
+import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.SortedSet;
+
+public class GestorNumerosDisponibles {
+	private SortedSet<String> lineas;
+	private IGenerador generador;
+	
+	public GestorNumerosDisponibles() {
+		this.lineas = new TreeSet<String>();
+		this.generador = new GeneradorUltimo();
+	}
+
+	public SortedSet<String> getLineas() {
+		return lineas;
+	}
+
+	public String obtenerNumeroLibre() {
+		return this.generador.obtenerNumeroLibre(this.lineas);
+	}
+
+	public void cambiarGenerador(IGenerador generador) {
+		this.generador = generador;
+	}
+}
